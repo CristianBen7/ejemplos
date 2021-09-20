acf85b6a3814acceaeef19a855d8cb47497bc0c5
CristianBen7 <80054298+CristianBen7@users.noreply.github.com>
2021-09-20 15:51:56 +0200

    Add files via upload

---
+++ Ej.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "es">
+
+	<body>   
+        
+        <h1>My first Web Page</h1>
+        <p>My first Paragraph</p>
+
+        <p id="demo"></p>
+        
+        <?php
+
+            echo 7+8;
+
+        ?>
+
+	</body>
+    
+</html>
